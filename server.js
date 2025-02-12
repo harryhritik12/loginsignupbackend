1132e795b5ad3c2592fec1d9c6e9c4333d7aa240
harryhritik <hritikpankal1999@gmail.com>
2025-02-12 16:33:06 +0530

    2

--- server.js
+++ server.js
@@ -14,8 +14,15 @@ const app = express();
 app.use(express.json());
 app.use(cors({
     origin: ["http://localhost:3000", "https://auth-tan-psi.vercel.app"],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
 }));
+app.use((req, res, next) => {
+  res.setHeader('Cross-Origin-Opener-Policy', 'unsafe-none');
+  next();
+});
+
 
 app.use(session({
     secret: process.env.SESSION_SECRET,
